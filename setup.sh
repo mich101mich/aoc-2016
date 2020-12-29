cb3e703e218f3765ab78fc79634a76ffdf29fd8e
mhillmann <mhillmann@uos.de>
2020-12-29 13:58:14 +0100

    added day 01

---
+++ setup.sh
@@ -0,0 +1,47 @@
+DAY=$1
+echo "use crate::utils::*;
+
+#[allow(unused)]
+pub fn run() {
+    #[allow(unused_variables)]
+    let input = include_str!(\"../input/$DAY.txt\");
+    // let input = "";
+    
+    let parsed = input
+        //.lines()
+        //.chars()
+        //.map(|l| l.chars().to_vec())
+        //.map(parse)
+        //.map(|l| scanf!(l, \"{}\", isize))
+        //.to_vec()
+        //.sum::<isize>()
+        //.parse::<isize>()
+        ;
+    
+    //pv!(parsed);
+    
+}" > src/days/day_$DAY.rs
+
+echo "#![allow(unused_imports)]
+
+#[macro_use]
+extern crate scan_fmt;
+
+#[macro_use]
+mod utils;
+mod days {
+    pub mod day_$DAY;
+}
+use days::day_$DAY;
+
+fn main() {
+    day_$DAY::run();
+}" > src/main.rs
+
+touch src/input/$DAY.txt
+
+if [ -f cmd.exe ]; then
+    cmd.exe /c code src/days/day_$DAY.rs src/input/$DAY.txt
+else
+    code src/days/day_$DAY.rs src/input/$DAY.txt
+fi
